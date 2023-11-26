c5f24e7cbf934cc8460e728a53393007ab836ff7
Buzaition <121060107+Buzaition@users.noreply.github.com>
2023-11-26 05:48:17 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ import { Configuration, OpenAIApi } from 'openai'
 dotenv.config()
 
 const configuration = new Configuration({
-    apiKey: "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",
+    apiKey:process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
